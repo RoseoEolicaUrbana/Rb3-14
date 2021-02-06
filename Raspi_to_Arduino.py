280c0cc98b1e521ac3fc3ecd7490984d6fed7c8f
oscar <proyectoroseo@gmail.com>
2021-02-06 03:18:01 +0100

    First python script

---
+++ Raspi_to_Arduino.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 03:15:42 2021
+
+@author: ai5
+"""
+
+import serial
+import RPi.GPIO as GPIO
+import time
+
+ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate=9600
+def blink(pin):
+
+
+GPIO.output(pin,GPIO.HIGH)  
+time.sleep(1)  
+GPIO.output(pin,GPIO.LOW)  
+time.sleep(1)  
+return
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+while True:
+
+read_ser=ser.readline()
+print(read_ser)
+if(read_ser=="Hello From Arduino!"):
+blink(11)
